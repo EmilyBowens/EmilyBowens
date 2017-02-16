aca1172737c9a0d6eb3ccf4f8b6b0f368ec42b0f
EmilyBowens <15181154@studentmail.ul.ie>
2017-02-16 11:44:37 +0000

    Update TextView

--- app/src/main/java/com/csis/lab04/MainActivity.java
+++ app/src/main/java/com/csis/lab04/MainActivity.java
@@ -1,21 +1,13 @@
 package com.csis.lab04; //package we're in
 
 //android imports
-import android.content.Context;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.KeyEvent;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
 import android.widget.CompoundButton;
-import android.widget.EditText;
-import android.widget.SeekBar;
 import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.TextView.OnEditorActionListener;
 import android.widget.Toast;
 
 //PURE DATA IMPORTS
@@ -31,12 +23,14 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 
+
 public class MainActivity extends AppCompatActivity {
 
     private PdUiDispatcher dispatcher; //must declare this to use later, used to receive data from sendEvents
 
-    @Override
     TextView myCounter;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);//Mandatory
         setContentView(R.layout.activity_main);//Mandatory
@@ -44,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
         Switch onOffSwitch = (Switch) findViewById(R.id.onOffSwitch);//declared the switch here pointing to id onOffSwitch
 
-        TextView myCounter = (TextView) findViewById(R.id.myCounter;
+        myCounter = (TextView) findViewById(R.id.counter);
 
         //Check to see if switch1 value changes
         onOffSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -93,6 +87,62 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    //<---THIS METHOD INITIALISES AUDIO SERVER----->
+    private void initPD() throws IOException {
+        int sampleRate = AudioParameters.suggestSampleRate(); //get sample rate from system
+        PdAudio.initAudio(sampleRate, 0, 2, 8, true); //initialise audio engine
+
+        dispatcher = new PdUiDispatcher(); //create UI dispatcher
+        PdBase.setReceiver(dispatcher); //set dispatcher to receive items from puredata patches
+
+        dispatcher.addListener("sendCounter", receiver1);
+        PdBase.subscribe("sendCounter");
+
+    }
+
+    private PdReceiver receiver1 = new PdReceiver() {
+
+        private void pdPost(final String msg) {
+            Log.e("RECEIVED:", msg);
+
+            new Handler().post(new Runnable() {
+                @Override
+                public void run() {
+                }
+            });
+        }
+
+        @Override
+        public void print(String s) {
+            Log.i("PRINT",s);
+            Toast.makeText(getBaseContext(),s, Toast.LENGTH_LONG);
+        }
+
+        @Override public void receiveBang(String source)
+        {
+            pdPost("bang");
+        }
+
+        @Override public void receiveFloat(String source, float x) {
+            pdPost("float: " + x);
+            if(source.equals("sendCounter")) {
+                myCounter.setText(String.valueOf(x));
+            }
+        }
+
+        @Override
+        public void receiveList(String source, Object... args) {
+            pdPost("list: " + Arrays.toString(args));
+        }
+
+        @Override public void receiveMessage(String source, String symbol, Object... args) {
+            pdPost("message: " + Arrays.toString(args));
+
+    }
+        @Override public void receiveSymbol(String source, String symbol) {
+            pdPost("symbol: " + symbol); } };
+
+
     //<---THIS METHOD LOADS SPECIFIED PATCH NAME----->
     private void loadPDPatch(String patchName) throws IOException
     {
@@ -106,19 +156,4 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
-
-    //<---THIS METHOD INITIALISES AUDIO SERVER----->
-    private void initPD() throws IOException
-    {
-        int sampleRate = AudioParameters.suggestSampleRate(); //get sample rate from system
-        PdAudio.initAudio(sampleRate,0,2,8,true); //initialise audio engine
-
-        dispatcher = new PdUiDispatcher(); //create UI dispatcher
-        PdBase.setReceiver(dispatcher); //set dispatcher to receive items from puredata patches
-
-        dispatcher.addListener("sendCounter",receiver1);
-        PdBase.subscribe("sendCounter");
-
-    }
-
 }
